2894af3c96a0f73a7a7957aec1f4938bc659bc0c
iHubG <ianmacalinao50@gmail.com>
2025-12-10 11:56:13 +0800

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import Button from './ui/button/Button.vue';
-import { Plus, LogOut, Smile } from 'lucide-vue-next';
+import { Plus, LogOut, Smile, KeyRound } from 'lucide-vue-next';
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 const props = defineProps<{
     handleAddDebtor: () => void
     handleLogout: () => void
+    handleChangePassword?: () => void
 }>()
 
-const username = ref("Ian");
+const name = ref("John Doe");
 
+const nameInitials = computed(() => {
+    return name.value
+        .split(" ")
+        .map(n => n[0])
+        .join("")
+        .toUpperCase();
+});
 </script>
 
 <template>
@@ -17,7 +33,7 @@ const username = ref("Ian");
         <div class="flex flex-col">
             <div class="flex items-center gap-2">
                 <h1 class="text-xl lg:text-2xl font-semibold text-gray-900">
-                    Welcome back, {{ username }}
+                    Welcome back, {{ name }}
                 </h1>
                 <Smile class="text-yellow-500 w-8 h-8" />
             </div>
@@ -27,16 +43,36 @@ const username = ref("Ian");
             </p>
         </div>
 
-
         <div class="flex items-center gap-2">
             <Button @click="props.handleAddDebtor" class="hidden sm:flex">
                 <Plus class="w-4 h-4 mr-2" />
                 Add New Debtor
             </Button>
 
-            <Button @click="props.handleLogout" variant="outline" size="icon">
-                <LogOut class="w-4 h-4" />
-            </Button>
+            <!-- Avatar + Dropdown -->
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <div
+                        class="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center cursor-pointer text-sm font-semibold">
+                        {{ nameInitials }}
+                    </div>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent class="w-48 mr-3">
+                    <DropdownMenuLabel>Account</DropdownMenuLabel>
+
+                    <DropdownMenuItem @click="props.handleChangePassword && props.handleChangePassword()"
+                        class="cursor-pointer">
+                        <KeyRound class="w-4 h-4 mr-2" />
+                        Change Password
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem @click="props.handleLogout" class="cursor-pointer">
+                        <LogOut class="w-4 h-4 mr-2" />
+                        Logout
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     </div>
 </template>
