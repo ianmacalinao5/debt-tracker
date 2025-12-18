ded2330e29de4402ee3fbdb0274767982f724a7e
iHubG <ianmacalinao50@gmail.com>
2025-12-18 23:58:27 +0800

    refactor: update password modal

--- src/components/modals/ChangePasswordModal.vue
+++ src/components/modals/ChangePasswordModal.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import Button from '../ui/button/Button.vue';
-import Input from '../ui/input/Input.vue';
-import { toast } from 'vue-sonner';
+import Button from "../ui/button/Button.vue";
+import Input from "../ui/input/Input.vue";
+import { toast } from "vue-sonner";
 
-// Import your new validation composable
-import { useChangePasswordValidation } from '@/composables/useChangePasswordValidation';
+import { useChangePasswordValidation } from "@/composables/useChangePasswordValidation";
 
 const {
     currentPassword,
@@ -13,7 +12,7 @@ const {
     currentPasswordMessage,
     newPasswordMessage,
     confirmPasswordMessage,
-    validate
+    validate,
 } = useChangePasswordValidation();
 
 const emit = defineEmits(["close"]);
@@ -26,22 +25,36 @@ const handleChangePassword = () => {
 };
 </script>
 
-
 <template>
     <div>
-        <form @submit.prevent="handleChangePassword" class="flex flex-col gap-5">
-
+        <form
+            @submit.prevent="handleChangePassword"
+            class="flex flex-col gap-5"
+        >
             <!-- Current Password -->
             <div class="flex flex-col gap-2">
-                <Input type="password" v-model="currentPassword" placeholder="Current Password" :class="{
-                    'border-red-500': currentPasswordMessage,
-                    'focus-visible:ring-red-200': currentPasswordMessage
-                }" />
+                <Input
+                    type="password"
+                    v-model="currentPassword"
+                    placeholder="Current Password"
+                    :class="{
+                        'border-red-500': currentPasswordMessage,
+                        'focus-visible:ring-red-200': currentPasswordMessage,
+                    }"
+                />
 
-                <transition enter-from-class="opacity-0 -translate-y-1" enter-to-class="opacity-100 translate-y-0"
-                    enter-active-class="transition-all duration-150" leave-from-class="opacity-100 translate-y-0"
-                    leave-to-class="opacity-0 -translate-y-1" leave-active-class="transition-all duration-150">
-                    <p v-if="currentPasswordMessage" class="text-[12px] ml-2 text-red-500">
+                <transition
+                    enter-from-class="opacity-0 -translate-y-1"
+                    enter-to-class="opacity-100 translate-y-0"
+                    enter-active-class="transition-all duration-150"
+                    leave-from-class="opacity-100 translate-y-0"
+                    leave-to-class="opacity-0 -translate-y-1"
+                    leave-active-class="transition-all duration-150"
+                >
+                    <p
+                        v-if="currentPasswordMessage"
+                        class="text-[12px] ml-2 text-red-500"
+                    >
                         {{ currentPasswordMessage }}
                     </p>
                 </transition>
@@ -49,15 +62,28 @@ const handleChangePassword = () => {
 
             <!-- New Password -->
             <div class="flex flex-col gap-2">
-                <Input type="password" v-model="newPassword" placeholder="New Password" :class="{
-                    'border-red-500': newPasswordMessage,
-                    'focus-visible:ring-red-200': newPasswordMessage
-                }" />
+                <Input
+                    type="password"
+                    v-model="newPassword"
+                    placeholder="New Password"
+                    :class="{
+                        'border-red-500': newPasswordMessage,
+                        'focus-visible:ring-red-200': newPasswordMessage,
+                    }"
+                />
 
-                <transition enter-from-class="opacity-0 -translate-y-1" enter-to-class="opacity-100 translate-y-0"
-                    enter-active-class="transition-all duration-150" leave-from-class="opacity-100 translate-y-0"
-                    leave-to-class="opacity-0 -translate-y-1" leave-active-class="transition-all duration-150">
-                    <p v-if="newPasswordMessage" class="text-[12px] ml-2 text-red-500">
+                <transition
+                    enter-from-class="opacity-0 -translate-y-1"
+                    enter-to-class="opacity-100 translate-y-0"
+                    enter-active-class="transition-all duration-150"
+                    leave-from-class="opacity-100 translate-y-0"
+                    leave-to-class="opacity-0 -translate-y-1"
+                    leave-active-class="transition-all duration-150"
+                >
+                    <p
+                        v-if="newPasswordMessage"
+                        class="text-[12px] ml-2 text-red-500"
+                    >
                         {{ newPasswordMessage }}
                     </p>
                 </transition>
@@ -65,15 +91,28 @@ const handleChangePassword = () => {
 
             <!-- Confirm Password -->
             <div class="flex flex-col gap-2">
-                <Input type="password" v-model="confirmPassword" placeholder="Confirm New Password" :class="{
-                    'border-red-500': confirmPasswordMessage,
-                    'focus-visible:ring-red-200': confirmPasswordMessage
-                }" />
+                <Input
+                    type="password"
+                    v-model="confirmPassword"
+                    placeholder="Confirm New Password"
+                    :class="{
+                        'border-red-500': confirmPasswordMessage,
+                        'focus-visible:ring-red-200': confirmPasswordMessage,
+                    }"
+                />
 
-                <transition enter-from-class="opacity-0 -translate-y-1" enter-to-class="opacity-100 translate-y-0"
-                    enter-active-class="transition-all duration-150" leave-from-class="opacity-100 translate-y-0"
-                    leave-to-class="opacity-0 -translate-y-1" leave-active-class="transition-all duration-150">
-                    <p v-if="confirmPasswordMessage" class="text-[12px] ml-2 text-red-500">
+                <transition
+                    enter-from-class="opacity-0 -translate-y-1"
+                    enter-to-class="opacity-100 translate-y-0"
+                    enter-active-class="transition-all duration-150"
+                    leave-from-class="opacity-100 translate-y-0"
+                    leave-to-class="opacity-0 -translate-y-1"
+                    leave-active-class="transition-all duration-150"
+                >
+                    <p
+                        v-if="confirmPasswordMessage"
+                        class="text-[12px] ml-2 text-red-500"
+                    >
                         {{ confirmPasswordMessage }}
                     </p>
                 </transition>
